7afaaeca0659ea6a533fff58b47932b136046e4e
Daniel McKeown <2014@pacificpelican.us>
2017-10-22 19:44:17 -0700

    gallery of lamp photos links to uploads at /photostream

--- application/views/gallery4_index_view.php
+++ application/views/gallery4_index_view.php
@@ -100,14 +100,22 @@
 </div>
 
 			<h2><?= $title ?></h2>
-			<p>
-				<? 	
+			<div class="contactSheetContainer">
+				<?	
 				$posts_latest_first = $posts;
-				?><table class="table hover stack"><thead><tr><td>file name</td><td>photo page</td><td>action</td></thead><tbody><?
+				?><table class="table hover stack"><tbody>
+        <?
 					foreach ($posts_latest_first as $key => $value) {
+            echo "<section class='oneImage'>";
+            echo "<main class='img_holder'>";
+              echo "<a class='img_link' href='". SITE_URL . "/image/" . $value['post_title'] . "'><img src='https://images.unsplash.com/photo-1464981093170-0a547e55c54c?dpr=1&auto=compress,format&fit=crop&w=2550&h=&q=80&cs=tinysrgb&crop='></img></a>";
+            echo "</main><aside class='img_caption'>";
+            echo "</aside>";
+          echo "</section>";
 						echo "<tr class='blog_posts_list'><td class='file_link'><a href='" . DROPBOXS_FILE_PATH_VIA_ROOT . $value['post_title'] . "'>" . $value['post_title'] . "</a></td><td class='file_link'><a href='". SITE_URL . "/image/" . $value['post_title'] . "'>" . "link" . "</a></td><td class='delete_link'><a href='/flickrs/kill/file/" . $value['post_id'] . "'><button type='button' class='alert button'><span class='delete_it'>delete</span></button></td></tr>";
-					} ?>
+          } 
+          ?>
 				</table>
-			</p>
+      </div>
 			
 		</div>
